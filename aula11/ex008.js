afcfdba1ff5c9ae6900abaae74cafa81be1f3516
kleberaluizio <kleberaluizio@gmail.com>
2024-03-22 15:17:58 -0300

    Verificando conhecimento sobre funcoes e condicoes

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,10 +1,7 @@
 var vel = 78.2
 console.log(`A velocidade do seu carro é ${vel} km/h`)
 
-var message;
 if(vel > 60){
-    message = 'Você ultrapassou a velocidade máxima permitida'
-} else {
-    message = 'Dirija sempre usando cinto de segurança'
-}
-console.log(message)
+    console.log('Você ultrapassou a velocidade máxima permitida')
+} 
+    console.log('Dirija sempre usando cinto de segurança')
